ec49802988a378c70a8ee4c322f2c8221d2c4b30
Ian Korf <ifkorf@gmail.com>
2021-02-11 09:58:46 -0500

    fixes cli

--- phic.py
+++ phic.py
@@ -13,12 +13,14 @@ if __name__ == '__main__':
 		description='Poission-based HMM Integer Classifier')
 	parser.add_argument('--file', required=True, type=str,
 		metavar='<file>', help='text file of values')
+	parser.add_argument('--classes', required=True, type=int, nargs='*',
+		metavar='<int>', help='at least 2 integers that define classes')
+	parser.add_argument('--names', required=False, type=str, nargs='*',
+		metavar='<str>', help='names corresponding to integers')
 	parser.add_argument('--background', required=False, type=str,
 		metavar='<file>', help='text file of background values')
-	parser.add_argument('--classes', required=True, type=int, nargs='*',
-		metavar='<int>', help='low expectation [%(default)i]')
 	parser.add_argument('--x', required=False, type=int, default=5,
-		metavar='<file>', help='switch cost [%(default)i]')
+		metavar='<file>', help='switch cost (in log-space) [%(default)i]')
 	parser.add_argument('--min', required=False, type=int, default=0,
 		metavar='<int>', help='limit minimum value [%(default)i]')
 	parser.add_argument('--max', required=False, type=int, default=255,
@@ -28,11 +30,11 @@ if __name__ == '__main__':
 	parser.add_argument('--window', required=False, type=int,
 		metavar='<int>', help='aggregate values into windows of <int> size')
 	parser.add_argument('--gff', required=False, type=str,
-		metavar='<str>', help='format as GFF (chr,off')
+		metavar='<str>', help='format as GFF (chr,off)')
 	parser.add_argument('--bed', required=False, type=str,
 		metavar='<str>', help='format as BED (chr,off)')
 	parser.add_argument('--keep', action='store_true',
-		help=f'keep temporary files ({ifile}, {ofile}')
+		help=f'keep temporary files ({ifile}, {ofile})')
 	arg = parser.parse_args()
 	
 	if len(arg.classes) < 2: raise Exception('need at least 2 classes')
@@ -50,6 +52,13 @@ if __name__ == '__main__':
 		off = int(off)
 	
 	if arg.background: raise Exception('background not implemented yet')
+	
+	name = {}
+	if arg.names:
+		if len(arg.names) != len(arg.classes):
+			raise Exception('must be same number of names and classes')
+		for i in range(len(arg.classes)):
+			name[arg.classes[i]] = arg.names[i]
 	
 	# collect data and threshold/scale as necessary
 	data = []
@@ -89,11 +98,12 @@ if __name__ == '__main__':
 	with open(ofile) as fp:
 		for line in fp.readlines():
 			beg, end, state = line.split()
-			output.append((int(beg), int(end), state))
+			output.append((int(beg), int(end), int(state)))
 	
 	for b, e, s in reversed(output):
 		r = statistics.mean(raw[b-1:e])
 		d = statistics.mean(data[b-1:e])
+		if arg.names: s = name[s]
 		if arg.window:
 			b = b * arg.window - arg.window + 1
 			e *= arg.window
